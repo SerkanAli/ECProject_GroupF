afe763d45b4070f83166443d920f4d0f3d1dde48
Serkan Ali Ergene <nerkani@outlook.com>
2018-12-30 17:32:43 +0100

    Create Blank Interface

---
+++ src/Interface/AmazonStorage.java
@@ -0,0 +1,34 @@
+package Interface;
+
+import java.io.*;
+import java.util.List;
+
+
+public class AmazonStorage  implements KeyValueInterface, Serializable  {
+
+
+    @Override
+    public Object getValue(String key) {
+        return null;
+    }
+
+    @Override
+    public List<String> getKeys() {
+        return null;
+    }
+
+    @Override
+    public void store(String key, Serializable value) {
+
+    }
+
+    @Override
+    public void delete(String key) {
+
+    }
+
+    @Override
+    public String  StorageName() {
+        return "Amazon";
+    }
+}
